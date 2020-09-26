2cac540f211037cfe365fd6f4d9f1e2f836fed65
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-09-26 14:29:52 -0400

    updateButton and skip func are added

--- Custom Video Player/scripts.js
+++ Custom Video Player/scripts.js
@@ -10,3 +10,12 @@ function togglePlay() {
   const method = video.paused ? "play" : "pause";
   video[method]();
 }
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  console.log(icon);
+  toggle.textContent = icon;
+}
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
