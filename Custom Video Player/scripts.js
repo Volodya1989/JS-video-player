fa3db24f23b4f39a09abd8f3850d8608b2419b07
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-09-26 14:26:31 -0400

    selectors added

--- Custom Video Player/scripts.js
+++ Custom Video Player/scripts.js
@@ -1,4 +1,12 @@
-function togglePlay(){
-    const method = video.paused ? 'play': 'pause';
-    video[method]();
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const progress = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
 }
